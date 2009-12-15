4f6bb4ca6a0d0aa2813269d9844d32e72980108b
Karl-Heinz Koether <khkoether@gmx.de>
2009-12-15 21:39:01 +0100

    Anpassen des Shell-Script an die neuen Datei-Namen: jruby, netbeans

--- iiu_de.sh
+++ iiu_de.sh
@@ -2,7 +2,7 @@
 #
 # Script zum Erstellen aller HTML-Dateien in iiu_de
 
-for paket in asciidoc git netbeans671 ruby19 jruby131
+for paket in asciidoc git netbeans ruby19 jruby
 do
   asciidoc -a toc ${paket}.txt
 done
