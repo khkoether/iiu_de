504bdcc4e4e8524c108bc07288686e3079bfd95e
Karl-Heinz Koether <khkoether@gmx.de>
2012-10-08 13:44:31 +0200

    Neue Programmversion: AsciiDoc, Ruby, OpenJDK 7, Oracle JDK 7, JRuby
    Veröffentlichung von Rails 3.2.8
    Die Beschreibung der Installation des JDK und NetBeans wird getrennt.
    Neue Dateien für die Beispiele zu Ruby on Rails:
    Ruby on Rails: Erste Schritte (mit Ruby)
    Ruby on Rails: Erste Schritte (mit JRuby)
    Das ChangeLog in den einzelnen Dateien wurde entfernt.

--- iiu_de.sh
+++ iiu_de.sh
@@ -2,9 +2,11 @@
 #
 # Script zum Erstellen aller HTML-Dateien in iiu_de
 
-for paket in asciidoc git netbeans ruby19 jruby
+for paket in asciidoc git ruby19 ruby19ror jdk netbeans jruby jrubyror
 do
-  asciidoc -a toc ${paket}.txt
+  #asciidoc -a toc ${paket}.txt
+  #asciidoc -b html5 -a icons -a toc2 -a theme=flask -a stylesheet=${MYPWD}/book/stylesheets/${NAMECSS} -o output/${name}.html $nameext
+  asciidoc -b html5 -a icons -a toc ${paket}.txt
 done
 
 
