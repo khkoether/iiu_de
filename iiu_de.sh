c881b19a7f11aeff0023a960825fc228c91b76cc
Karl-Heinz Koether <khkoether@gmx.de>
2012-10-08 14:21:30 +0200

    HTML-Seiten werden in der Reihenfolge der Installation der Programme erzeugt.

--- iiu_de.sh
+++ iiu_de.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 #
 # Script zum Erstellen aller HTML-Dateien in iiu_de
-
-for paket in asciidoc git ruby19 ruby19ror jdk netbeans jruby jrubyror
+#
+for paket in asciidoc git ruby19 ruby19ror jdk jruby jrubyror netbeans
 do
   #asciidoc -a toc ${paket}.txt
   #asciidoc -b html5 -a icons -a toc2 -a theme=flask -a stylesheet=${MYPWD}/book/stylesheets/${NAMECSS} -o output/${name}.html $nameext
