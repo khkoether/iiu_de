1c547dbd03fc60b1ee4fadb3334a9e7b6bed217d
Karl-Heinz Koether <khkoether@gmx.de>
2013-10-17 20:29:26 +0200

    Neue Programmversion: Git, Ruby (1.9.3, 2.0.0), Java, JRuby, NetBeans Beispiele zu: AsciiDoc, Rails 4 mit [Ruby, JRuby, NetBeans]

--- iiu_de.sh
+++ iiu_de.sh
@@ -2,11 +2,11 @@
 #
 # Script zum Erstellen aller HTML-Dateien in iiu_de
 #
-for paket in asciidoc git ruby19 ruby19ror jdk jruby jrubyror netbeans
+for paket in asciidoc asciidoc_1st git ruby19 ruby20 ruby20_1st jdk jruby jruby_1st netbeans netbeans_1st
 do
   #asciidoc -a toc ${paket}.txt
   #asciidoc -b html5 -a icons -a toc2 -a theme=flask -a stylesheet=${MYPWD}/book/stylesheets/${NAMECSS} -o output/${name}.html $nameext
-  asciidoc -b html5 -a icons -a toc ${paket}.txt
+  asciidoc -b html5 -a icons -a toc -a stylesheet=/home/khk/80/Books/CSS/mybooks.css ${paket}.txt
 done
 
 
