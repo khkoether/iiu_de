66fc7fd6b7800044b9d6f6fcf35da11d3c963956
Karl-Heinz Koether <khkoether@gmx.de>
2014-03-08 12:54:27 +0100

    Neue Programmversion: Git, Ruby (1.9.3, 2.0.0), Java, JRuby, NetBeans Beispiele zu: AsciiDoc, Rails 4 mit [Ruby, JRuby, NetBeans] Die AsciiDoc-Quell-Dateien wurden alle auf das Suffix .adoc umbenannt. Die HTML-Seiten werden jetzt mit Asciidoctor (v1.5.0-Preview3) erstellt.

--- iiu_de.sh
+++ iiu_de.sh
@@ -4,9 +4,12 @@
 #
 for paket in asciidoc asciidoc_1st git ruby19 ruby20 ruby20_1st jdk jruby jruby_1st netbeans netbeans_1st
 do
-  #asciidoc -a toc ${paket}.txt
-  #asciidoc -b html5 -a icons -a toc2 -a theme=flask -a stylesheet=${MYPWD}/book/stylesheets/${NAMECSS} -o output/${name}.html $nameext
-  asciidoc -b html5 -a icons -a toc -a stylesheet=/home/khk/80/Books/CSS/mybooks.css ${paket}.txt
+  echo $paket
+  #asciidoc (python)
+  #asciidoc -b html5 -a icons -a toc ${paket}.adoc
+  
+  #asciidoc (ruby)
+  asciidoctor -b html5 -a toc ${paket}.adoc
 done
 
 
