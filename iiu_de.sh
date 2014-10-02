dd1af5a14d9116bf5fb124c95c06e10f4e6ec686
Karl-Heinz Koether <khkoether@gmx.de>
2014-10-02 22:54:26 +0200

    Neue Programmversion: Asciidoctor, Git (2.1.x), Ruby (1.9.3, 2.1.x), Java, JRuby, NetBeans Beispiele zu: Asciidoctor, Rails (4.2.x) mit [Ruby, JRuby, NetBeans] Die Seite zum installieren von AsciiDoc (python) wird nicht mehr aktualisiert. Ruby 2.0.x hat im Umfeld von Rails keine Bedeutung und wird ebenfalls nicht mehr aktualisiert.

--- iiu_de.sh
+++ iiu_de.sh
@@ -2,14 +2,14 @@
 #
 # Script zum Erstellen aller HTML-Dateien in iiu_de
 #
-for paket in asciidoc asciidoc_1st git ruby19 ruby20 ruby20_1st jdk jruby jruby_1st netbeans netbeans_1st
+for paket in index asciidoctor asciidoctor_1st asciidoc git ruby19 ruby20 ruby21 ruby21_1st jdk jruby jruby_1st netbeans netbeans_1st
 do
   echo $paket
   #asciidoc (python)
   #asciidoc -b html5 -a icons -a toc ${paket}.adoc
   
-  #asciidoc (ruby)
-  asciidoctor -b html5 -a toc ${paket}.adoc
+  #asciidoc (ruby): html5 is default
+  asciidoctor -a toc ${paket}.adoc
 done
 
 
