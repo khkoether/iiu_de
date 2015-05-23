94c8d86d5ba398194eadf72a6ca6f31eb15130e0
Karl-Heinz Koether <khkoether@gmx.de>
2015-05-23 22:20:01 +0200

    Neue Programmversion: Ruby (2.2.2), JRuby (1.7.20), NetBeans (8.0) (JRuby 1.7.20), Beispiele zu: Rails (4.2.1) mit [Ruby, JRuby, NetBeans]

--- iiu_de.sh
+++ iiu_de.sh
@@ -2,13 +2,13 @@
 #
 # Script zum Erstellen aller HTML-Dateien in iiu_de
 #
-for paket in index asciidoctor asciidoctor_1st asciidoc git ruby19 ruby20 ruby21 ruby21_1st jdk jruby jruby_1st netbeans netbeans_1st
+for paket in index asciidoctor asciidoctor_1st asciidoc git ruby22 ruby22_1st jdk jruby jruby_1st netbeans8 netbeans8_1st
 do
   echo $paket
   #asciidoc (python)
   #asciidoc -b html5 -a icons -a toc ${paket}.adoc
   
-  #asciidoc (ruby): html5 is default
+  #asciidoctor (ruby): html5 is default
   asciidoctor -a toc ${paket}.adoc
 done
 
