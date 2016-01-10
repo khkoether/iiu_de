4b99a935edfdd205ca376b18df92fe6cd78bf9b4
Karl-Heinz Koether <khkoether@gmx.de>
2016-01-10 11:01:12 +0100

    Neue Programmversionen: Asciidoctor (1.5.4), Git (2.7.0)), Ruby (2.2.4), Ruby (2.3.0), Rails (4.2.5), JDK (7/8), JRuby (1.7.23) Beispiele zu: Rails (4.2.5) mit [Ruby/ JRuby] Unverändert:  NetBeans (8.0), NetBeans: Erste Schritte... (Rails 4.2.1)

--- iiu_de.sh
+++ iiu_de.sh
@@ -1,15 +1,44 @@
 #! /bin/bash
 #
-# Script zum Erstellen aller HTML-Dateien in iiu_de
+# Script zum Erstellen aller HTML-Seiten in iiu_de
 #
-for paket in index asciidoctor asciidoctor_1st asciidoc git ruby22 ruby22_1st jdk jruby jruby_1st netbeans8 netbeans8_1st
+ME=iiu_de.sh
+
+echo "== HTML-Seiten werden erstellt:" 
+for seite in index asciidoctor asciidoctor_1st git 
 do
-  echo $paket
+  echo "${seite}.html"
   #asciidoc (python)
-  #asciidoc -b html5 -a icons -a toc ${paket}.adoc
+  #asciidoc -b html5 -a icons -a toc ${seite}.adoc
   
   #asciidoctor (ruby): html5 is default
-  asciidoctor -a toc ${paket}.adoc
+  asciidoctor -a toc ${seite}.adoc
 done
 
+for seite in ruby22 ruby_on_rails42 ruby23 
+do
+  echo "${seite}.html"
+  asciidoctor -a toc ${seite}.adoc
+done
+
+for seite in jdk jruby jruby_on_rails42 netbeans8 netbeans8_1st
+do
+  echo "${seite}.html"
+  asciidoctor -a toc ${seite}.adoc
+done
+
+#
+# Alt-Seiten
+#
+echo
+echo "== Die folgenden Seiten werden nicht weiter von mir gepflegt:"
+echo "asciidoc ruby19 ruby20 ruby21 netbeans7 netbeans7_1st"
+#for seite in asciidoc ruby19 ruby20 ruby21 netbeans7 netbeans7_1st   
+#do
+#  echo "${seite}.html"
+#  asciidoctor -a toc ${seite}.adoc
+#done
+
+echo 
+echo "Ende: ${ME}"
 
