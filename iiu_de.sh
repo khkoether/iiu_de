39d27fd17bb8263ebb91e906b883fa5ff8cff2f3
k-h köther <khkoether@gmx.de>
2018-12-16 19:22:49 +0100

    Neue Programmversionen: 5.2.0
    Asciidoctor (1.5.8), Git (2.20.0),
    Ruby (2.5.3), Ruby on Rails (5.2),
    JRuby 9k (9.2.5.0), JRuby 9k on Rails (5.2)

--- iiu_de.sh
+++ iiu_de.sh
@@ -15,33 +15,41 @@ do
   asciidoctor -a toc ${seite}.adoc
 done
 
-for seite in chruby git
+
+for seite in chruby
 do
   echo "${seite}.html"
   asciidoctor -a toc ${seite}.adoc
 done
 
-for seite in ruby23 ruby24 ruby25 ruby_on_rails51
+for seite in git
 do
   echo "${seite}.html"
   asciidoctor -a toc ${seite}.adoc
 done
 
-for seite in jdk jruby9k jruby9k_on_rails51
+for seite in ruby25 ruby_on_rails52
 do
   echo "${seite}.html"
   asciidoctor -a toc ${seite}.adoc
 done
+
+for seite in jdk jruby9k jruby9k_on_rails52
+do
+  echo "${seite}.html"
+  asciidoctor -a toc ${seite}.adoc
+done
+
 
 #
 # Alt-Seiten
 #
 echo
 echo "== Die folgenden Seiten werden nicht weiter von mir gepflegt:"
-echo "asciidoc ruby22 ruby_on_rails42 jruby jruby_on_rails42 netbeans8 netbeans8_1st"
+echo "asciidoc ruby22 ruby23 ruby24 ruby_on_rails42 ruby_on_rails51 jruby jruby_on_rails42 netbeans8 netbeans8_1st jruby_on_rails51"
 
 echo "...auskommentiert..."
-#for seite in asciidoc ruby ruby_on_rails42 jruby jruby_on_rails42 netbeans8 netbeans8_1st
+#echo "asciidoc ruby22 ruby23 ruby24 ruby_on_rails42 ruby_on_rails51 jruby jruby_on_rails42 netbeans8 netbeans8_1st jruby_on_rails51"
 #do
 #  echo "${seite}.html"
 #  asciidoctor -a toc ${seite}.adoc
